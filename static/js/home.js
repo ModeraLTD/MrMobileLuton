cdb8b6a1e54a7b6d8a16d8253dba85fc73c17bf7
Cyclip <40525455+Cyclip@users.noreply.github.com>
2021-06-10 20:26:28 +0100

    Complete guarantee section
    Changed repair.png

--- static/js/home.js
+++ static/js/home.js
@@ -1,15 +1,17 @@
 // Handle the background scroll blur
 function handleBgBlur(scroll) {
-	val = Math.ceil((scroll/blurScrollMax) * blurPxMax);
-	if (Math.abs(val-currentBlur) > changeThreshold) {
-		currentBlur = val;
-		var filterVal = `blur(${currentBlur}px)`;
-		$('#bgimg')
-			.css('filter',filterVal)
-			.css('webkitFilter',filterVal)
-			.css('mozFilter',filterVal)
-			.css('oFilter',filterVal)
-			.css('msFilter',filterVal);
+	if (scroll <= blurScrollMax) {
+		val = Math.ceil((scroll/blurScrollMax) * blurPxMax);
+		if (Math.abs(val-currentBlur) > changeThreshold) {
+			currentBlur = val;
+			var filterVal = `blur(${currentBlur}px)`;
+			$('#bgimg')
+				.css('filter',filterVal)
+				.css('webkitFilter',filterVal)
+				.css('mozFilter',filterVal)
+				.css('oFilter',filterVal)
+				.css('msFilter',filterVal);
+		}
 	}
 }
 
@@ -42,6 +44,36 @@ function handleScroll(scroll) {
 	handleBgBlur(Math.ceil(scroll / 10) * 10);
 }
 
+function updateQuotes(d) {
+	quotesIndex += d;
+	max = quotes.length - 1;
+
+	if (quotesIndex > max) {
+		quotesIndex = 0;
+	} else if (quotesIndex < 0) {
+		quotesIndex = max;
+	}
+
+	_updateQuotes(true);
+}
+
+function _updateQuotes(flash) {
+	author = quotes[quotesIndex][0];
+	quote = quotes[quotesIndex][1];
+
+	if (flash) {
+		$(".quotes").fadeOut(200);
+		setTimeout(function() {
+			$("#author").text(author);
+			$("#quote").text(quote);
+			$(".quotes").fadeIn(200);
+		}, 200);
+	} else {
+		$("#author").text(author);
+		$("#quote").text(quote);
+	}
+}
+
 // When the page loads
 $(document).ready(function() {
 	$(".fadein").hide();
@@ -51,10 +83,36 @@ $(document).ready(function() {
 		var scroll = $(window).scrollTop();
 		handleScroll(scroll);
 	});
+
+	_updateQuotes(false);
 });
 
 cs_visible = false;
 currentBlur = 0;
+quotesIndex = 0;
+
+quotes = [
+	[
+		"Junaid Rahmat",
+		"Best shop in Luton most affordable prices in the bury park I got these two phones from them literally I loved it both works excellent thanks mate ."
+	],
+	[
+		"Ravi Sakma",
+		"Professional technician in Luton... best customer service ❤️"
+	],
+	[
+		"Zubair Bond",
+		"Better than all shops in luton.."
+	],
+	[
+		"Terry Woods",
+		"TOP CLASS.....Don't go anywhere else, this is the best."
+	],
+	[
+		"Mirjan Noka",
+		"Positive: Professionalism"
+	]
+];
 
 const blurScrollMax = 400;
 const blurPxMax = 8;
