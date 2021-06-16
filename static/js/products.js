a07b1a60f2af646637269651e1f5108882814d51
Cyclip <40525455+Cyclip@users.noreply.github.com>
2021-06-16 15:33:07 +0100

    Add product tab functionality
    Including query parameter "tab"
    No select class in general

--- static/js/products.js
+++ static/js/products.js
@@ -1,7 +1,142 @@
 // On page load
 $(document).ready(function() {
+	params = new URLSearchParams(window.location.search);
+	tab = params.get("tab");
+	console.log(tab);
+	if (tab == null) {
+		// fill in apple as default
+		addProducts(products["apple"]);
+		renameTabname("APPLE");
+	} else {
+		addProducts(products[tab]);
+		renameTabname(tab.toUpperCase());
+	}
+
+	spaceOutTab();
+
+	$("#phones-apple").click(function() {
+		removeProducts();
+		fadeOutPage();
+		setTimeout(function() {
+			addProducts(products["apple"]);
+			renameTabname("apple");
+			fadeInPage();
+			spaceOutTab();
+		}, 400);
+	});
+
+	$("#phones-samsung").click(function() {
+		removeProducts();
+		fadeOutPage();
+		setTimeout(function() {
+			renameTabname("samsung");
+			spaceOutTab();
+			addProducts(products["samsung"]);
+			setTimeout(fadeInPage, 400);
+		}, 200);
+	});
+
+	fadeInPage();
+});
+
+function removeProducts() {
+	$(".products").empty();
+}
+
+function addProducts(productArray) {
+	finalhtml = "";
+	for (productIndex in productArray) {
+		product = productArray[productIndex];
+
+		// If img is none use default
+		img = product["img"];
+		name = product["name"];
+		cost = product["cost"];
+
+		$(".products").append(`<div class="product noselect">
+	<img src="${img}"/>
+	<h2 class="name">${name}</h2>
+	<h2 class="cost">${cost}</h2>
+</div>
+`);
+	}
+}
+
+function renameTabname(tabname) {
+	target = $(".col.page .toptext.tab");
+	target.text(tabname.toUpperCase());
+	target.addClass("unspaced");
+}
+
+function spaceOutTab() {
 	setTimeout(
-		function() {$(".toptext").removeClass("unspaced");},
+		function() {$(".col.page .toptext").removeClass("unspaced");},
 		500
 	);
-});
+}
+
+function fadeInPage() {
+	$(".col.page").fadeIn(200);
+}
+
+function fadeOutPage() {
+	$(".col.page").fadeOut(200);
+}
+
+// constants
+const products = {
+	"samsung": [
+		{
+			"img": "http://localhost:5000/static/css/imgs/placeholder-product.png",
+			"name": "Samsung product",
+			"cost": "0.00"
+		},
+		{
+			"img": "http://localhost:5000/static/css/imgs/placeholder-product.png",
+			"name": "Example product",
+			"cost": "0.00"
+		},
+		{
+			"img": "http://localhost:5000/static/css/imgs/placeholder-product.png",
+			"name": "Example product",
+			"cost": "0.00"
+		},
+		{
+			"img": "http://localhost:5000/static/css/imgs/placeholder-product.png",
+			"name": "Example product",
+			"cost": "0.00"
+		},
+		{
+			"img": "http://localhost:5000/static/css/imgs/placeholder-product.png",
+			"name": "Example product",
+			"cost": "0.00"
+		}
+	],
+	"apple": [
+		{
+			"img": "http://localhost:5000/static/css/imgs/placeholder-product.png",
+			"name": "Apple product",
+			"cost": "0.00"
+		},
+		{
+			"img": "http://localhost:5000/static/css/imgs/placeholder-product.png",
+			"name": "Example product",
+			"cost": "0.00"
+		},
+		{
+			"img": "http://localhost:5000/static/css/imgs/placeholder-product.png",
+			"name": "Example product",
+			"cost": "0.00"
+		},
+		{
+			"img": "http://localhost:5000/static/css/imgs/placeholder-product.png",
+			"name": "Example product",
+			"cost": "0.00"
+		},
+		{
+			"img": "http://localhost:5000/static/css/imgs/placeholder-product.png",
+			"name": "Example product",
+			"cost": "0.00"
+		}
+	]
+}
