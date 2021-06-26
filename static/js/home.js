8604da50113adb089ae0c9c9dc2fe5bdc63617fc
Cyclip <40525455+Cyclip@users.noreply.github.com>
2021-06-26 21:25:06 +0100

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -244,42 +244,42 @@ quotes = [
 	]
 ];
 
-products = {
+const products = {
 	"apple": [
 		{
-			"img": "https://img.icons8.com/ios-glyphs/100/000000/iphone.png",
+			"img": "https://www.chorleyxchange.co.uk/wp-content/uploads/2020/08/s-l1600.png",
 			"name": "iPhone 7 (128GB)",
 			"cost": "145"
 		},
 		{
-			"img": "https://img.icons8.com/ios-glyphs/100/000000/iphone.png",
+			"img": "https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP726/SP726-iphone6s-gray-select-2015.png",
 			"name": "iPhone 6s (64GB)",
 			"cost": "100"
 		},
 		{
-			"img": "https://img.icons8.com/ios-glyphs/100/000000/iphone.png",
+			"img": "https://cdn.simplyfixit.co.uk/wp-content/uploads/deviceImages/devicepng/apple/phone/iphone-x.png",
 			"name": "iPhone X (64GB)",
 			"cost": "260"
 		},
 		{
-			"img": "https://img.icons8.com/ios-glyphs/100/000000/iphone.png",
+			"img": "https://i.ebayimg.com/images/g/-NgAAOSwfVtd1HoC/s-l640.png",
 			"name": "iPhone XS Max (64GB)",
 			"cost": "350"
 		},
 	],
 	"samsung": [
 		{
-			"img": "https://img.icons8.com/ios-glyphs/100/000000/iphone.png",
+			"img": "https://image.samsung.com/africa_en/smartphones/galaxy-s9/shop/buyingtool/product/product_galaxys9_lilacpurple_01.png",
 			"name": "Samsung S9",
 			"cost": "170"
 		},
 		{
-			"img": "https://img.icons8.com/ios-glyphs/100/000000/iphone.png",
+			"img": "https://www.chitterchatter.co.uk/pub/media/catalog/product/cache/1b4929b11a49fe9bc0938cef0890c9e3/s/m/sm-g975fzwdbtu_1_largeproductimage.png",
 			"name": "Samsung S10",
 			"cost": "270"
 		},
 		{
-			"img": "https://img.icons8.com/ios-glyphs/100/000000/iphone.png",
+			"img": "https://www.buymobiles.net/files/images/handsets/samsung/samsung-galaxy-s20-fe-front-navy.png",
 			"name": "Samsung S20",
 			"cost": "410"
 		},
